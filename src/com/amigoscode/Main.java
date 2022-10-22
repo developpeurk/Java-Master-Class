bea1b5ae6fbad611e066d10fe3a9a43052dd1594
developpeurk <developpeurk@gmail.com>
2022-10-22 12:27:09 +0100

    2 - Your First Java Program

--- src/com/amigoscode/Main.java
+++ src/com/amigoscode/Main.java
@@ -2,6 +2,7 @@ package com.amigoscode;
 
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        System.out.println("Hello Java 🎉");
+        System.out.println(10+10);
     }
 }
